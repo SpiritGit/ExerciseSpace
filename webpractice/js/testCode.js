b3eeb53aed885e7ffefdec8351052b43fd7c4a99
Spirit <spirit@MacBook-Pro.local>
2020-06-09 19:38:49 +0800

    StudyProcess

---
+++ webpractice/js/testCode.js
@@ -0,0 +1,14 @@
+function testCode(n){
+    var arr = [];
+    for(var i = 0; i < n; i++){
+        var num = parseInt(Math.random()*123);
+        if (num >= 0 && num <= 9){
+            arr.push(num);
+        }else if(num >= 97 && num <= 122 || num >=65 && num <=90){
+            arr.push(String.fromCharCode(num));
+        }else{
+            i--;
+        }
+    }
+    return arr.join('');
+}
